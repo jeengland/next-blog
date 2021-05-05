e3dc53da1b3b9c2f2372a95fd16db6475b721a71
Jacob Cavazos-England <jacob.e.england@gmail.com>
2021-05-05 16:30:46 -0400

    Polished header and footer; added dark mode toggle

--- next-blog/components/common/Header.tsx
+++ next-blog/components/common/Header.tsx
@@ -1,12 +1,32 @@
-import { Heading, Flex } from "@chakra-ui/react";
+import { Avatar, Heading, Flex, Icon } from "@chakra-ui/react";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 import Link from "next/link";
+
+import ColorModeToggle from "./ColorModeToggle";
 
 export default function Header() {
   return (
-    <Flex justify="center">
-      <Link href="/">
-        <Heading cursor="pointer">What I'm Up To</Heading>
-      </Link>
+    <Flex
+      height="3rem"
+      p="0.5rem"
+      as="header"
+      justify="space-between"
+      width="100%"
+    >
+      <Flex>
+        <Avatar
+          name="Jacob Cavazos-England"
+          src="https://avatars.githubusercontent.com/u/47133136?v=4"
+        />
+        <Flex as="nav" fontSize="xxx-large">
+          <Icon as={FaLinkedin} ml="5px" />
+          <Icon as={FaGithub} ml="5px" />
+        </Flex>
+      </Flex>
+      <Heading cursor="pointer" ml="-100px">
+        <Link href="/">What I'm Up To</Link>
+      </Heading>
+      <ColorModeToggle />
     </Flex>
   );
 }
